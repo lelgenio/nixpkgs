50660d755d8c4fab0a47f63fa7ba8749dde08f3d
lelgenio <lelgenio@lelgenio.com>
2024-06-15 19:39:54 -0300

    nodePackages: remove uglify-js

--- pkgs/development/node-packages/aliases.nix
+++ pkgs/development/node-packages/aliases.nix
@@ -155,6 +155,7 @@ mapAliases {
   inherit (pkgs) titanium; # added 2023-08-17
   triton = pkgs.triton; # Added 2023-05-06
   typescript = pkgs.typescript; # Added 2023-06-21
+  inherit (pkgs) uglify-js; # added 2024-06-15
   inherit (pkgs) ungit; # added 2023-08-20
   inherit (pkgs) vsc-leetcode-cli; # Added 2023-08-30
   vscode-langservers-extracted = pkgs.vscode-langservers-extracted; # Added 2023-05-27
--- pkgs/development/node-packages/main-programs.nix
+++ pkgs/development/node-packages/main-programs.nix
@@ -55,7 +55,6 @@
   svelte-language-server = "svelteserver";
   teck-programmer = "teck-firmware-upgrade";
   typescript-language-server = "typescript-language-server";
-  uglify-js = "uglifyjs";
   undollar = "$";
   vscode-css-languageserver-bin = "css-languageserver";
   vscode-html-languageserver-bin = "html-languageserver";
